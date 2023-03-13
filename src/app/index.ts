11fe23bcdc3097ed0403e7327efb457922917b29
Adetayo1999 <adetayoomotomiwa99@gmail.com>
2023-03-13 02:16:42 +0100

    tested the app routing

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,17 @@
-import express from "express";
+import express from 'express';
+import morgan from 'morgan';
 
 export const getApp = () => {
   const app = express();
+
+  // middlewares
+  app.use(morgan('dev'));
+
+  app.get('/', (_, response) => {
+    response.send({
+      message: 'Hi there, welcome to Lexitar Gadgets',
+    });
+  });
 
   return app;
 };
