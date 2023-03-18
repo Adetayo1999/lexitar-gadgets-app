5b98e91f3abed49209c6e7989a33ecb508326bfc
Adetayo1999 <adetayoomotomiwa99@gmail.com>
2023-03-18 04:01:53 +0100

    fixed the bug of app starting without correct env vars

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { ENV } from '@/common/config/env';
 import connectDB from '@/db/index';
 
 const startUp = async () => {
-  if (Object.keys(ENV).some((env) => env === undefined)) {
+  if (Object.values(ENV).some((env) => env === undefined)) {
     throw new Error('ENVIRONMENT ERROR');
   }
 
